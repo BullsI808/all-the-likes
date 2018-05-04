b056d75f6149ddb8592b222de686d8d7c806c131
BullsI808 <JBasuil8697@myhta.org>
2018-05-03 14:38:08 -1000

    finished with te base code

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+    var likeCount = localStorage.getItem('like');
+    var loveCount = localStorage.getItem('love');
+    var laughCount = localStorage.getItem('laugh');
+    var likeImage = document.getElementById('like-image');
+    var loveImage = document.getElementById('love-image');
+    var laughImage = document.getElementById('laugh-image');
+    var likeText = document.getElementById('like-count');
+    var loveText = document.getElementById('love-count');
+    var laughText = document.getElementById('laugh-count');
+    var btn = document.getElementById('btn1');
+    likeImage.addEventListener('click', function() {
+        likeCount++;
+        localStorage.setItem('like' , likeCount);
+        likeText.innerHTML = likeCount;
+    });
+    loveImage.addEventListener('click', function() {
+        loveCount++;
+        localStorage.setItem('love' , loveCount);
+        loveText.innerHTML = loveCount;
+    });
+    laughImage.addEventListener('click', function() {
+        laughCount++;
+        localStorage.setItem('laugh' , laughCount);
+        laughText.innerHTML = laughCount;
+    });
+    btn.addEventListener('click' , function(){
+        localStorage.clear();
+        window.location.reload(false);
+    });
+};
